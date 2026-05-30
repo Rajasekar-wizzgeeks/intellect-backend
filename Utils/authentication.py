5348b160fb2db0bf358aa8cb1bcf3ad7537d5869
samuvel <samuvel@wizzgeeks.com>
2026-05-30 10:58:51 +0530

    changes in authentication

--- Utils/authentication.py
+++ Utils/authentication.py
@@ -9,8 +9,8 @@ import os
 
 class Authentication:
     def __init__(self):
-        self.secret_key = os.getenv("SECRET_KEY")
-        self.algorithm = os.getenv("ALGORITHM")
+        self.secret_key = os.getenv("SECRET_KEY","03db4eca8958213834ddf3aced797eaa081a78a68f68186fca78d9252ac84e17")
+        self.algorithm = os.getenv("ALGORITHM","HS256")
         self.expires_in = int(os.getenv("ACCESS_TOKEN_EXPIRE_HOURS","24"))
         self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
         
